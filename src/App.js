d7eefc0b6d4cbc60e2fb575e6cc39eda320597ca
EnricoGiovanno8 <genrico189@gmail.com>
2021-10-04 16:44:10 +0700

    As a user, I want to see a product list which I can paginate

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react'
 
+// React Router Dom
+import { Switch, Route } from 'react-router-dom'
+import HomePage from './pages/home';
+import LoginPage from './pages/login';
+import RegisterPage from './pages/register';
+
 class App extends React.Component {
   render () {
     return (
-      <div>
-        <h1>Hello World</h1>
-      </div>
+      <Switch>
+        <Route path="/" component={HomePage} exact />
+        <Route path="/login" component={LoginPage} />
+        <Route path="/register" component={RegisterPage} />
+      </Switch>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// React Router Dom
+import { BrowserRouter } from 'react-router-dom'
+
 ReactDOM.render(
-  <App />,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
   document.getElementById('root')
 );
